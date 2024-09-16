5d88fa186cf34931f5da38cb5fa80a7a69570f51
Kirill Karpychev <chromeinquiry@mail.ru>
2024-09-16 23:39:10 +0500

    modified:   main.cpp add Author, group number, date

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
-//Программа является учебной
+//This program is just an example one under VCS.
+//Author: Kirill Karpychev, KE-317, 16.09.2024
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    system("chcp 1251");
-    system("cls");
     int x, y;
-    cout << "Введите числа:\n";
+    cout << "Enter the numbers:\n";
     cout << "x = ";
     cin >> x;
     cout << "y = ";
     cin >> y;
-    cout << "Сумма = " << x + y << "\nРазность = " << x - y;
+    cout << "x + y = " << x + y << "\nx - y = " << x - y << "\nx * y = " << x * y;
     return 0;
 }
